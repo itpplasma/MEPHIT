39337e9306d6d38f958c6a2951457118668cb61f
Christopher Albert <albert@tugraz.at>
2017-09-12 16:17:39 +0200

    Makefile removed implicit-none, magdif improved, symlinks added

--- Makefile
+++ Makefile
@@ -6,13 +6,21 @@ PROJLIBS = $(HOME)/CODE/Libs
 ifeq ($(TOOLCHAIN),intel)
 	FC := ifort
 	CC := icc
-	FCFLAGS = -c -g -O0 -mkl -check bounds -implicitnone
-	CCFLAGS = -c -g -O0 -mkl
+	FCFLAGS = -c -g -O0 -mkl -traceback -check bounds #-implicitnone
+	CCFLAGS = -c -g -O0 -mkl -traceback 
 else ifeq ($(TOOLCHAIN),gnu)
 	FC := gfortran
 	CC := gcc
-	FCFLAGS = -c -g -Og -Wall -fbacktrace -fcheck=bounds -fimplicit-none
+	FCFLAGS = -c -g -Og -Wall -fbacktrace -fcheck=bounds #-fimplicit-none
 	CCFLAGS = -c -g -Og -Wall
+	PROJLIBS := $(PROJLIBS)/GNU
+else ifeq ($(TOOLCHAIN),sgi)
+	FC := gfortran
+	#FC := f90
+	CC := gcc
+	FCFLAGS = -c -g -Wall -fbacktrace -fcheck=bounds 
+        #FCFLAGS = -c -g -O0 -Wall
+	CCFLAGS = -std=c99 -c -g -O0 -Wall
 	PROJLIBS := $(PROJLIBS)/GNU
 endif
 
@@ -27,19 +35,33 @@ SUITESPARSE_LIB = $(SUITESPARSE_DIR)/lib
 SUITESPARSE_HDR = $(SUITESPARSE_DIR)/include
 SUITESPARSE_F90 = $(SUITESPARSE_DIR)/UMFPACK/Demo
 
+ifeq ($(OS),Windows_NT)
+	SUPERLU_DIR     = /home/chral/src/SuperLU_5.2.1
+	SUITESPARSE_LIB = /usr/lib
+	SUITESPARSE_HDR = /usr/include/suitesparse
+	SUITESPARSE_F90 = /usr/src/umfpack-5.7.4-1.src/umfpack-5.7.4/Demo
+endif
+
 # link flags
 LDFLAGS = -L $(SUITESPARSE_LIB) -L $(SUPERLU_LIB) -lsuperlu -lumfpack -lamd -lcholmod \
-          -lcolamd -lcamd -lmetis -lccolamd  
+          -lcolamd -lcamd -lccolamd  
 
 ifeq ($(TOOLCHAIN),intel)
 	LDFLAGS += -limf -lirc -mkl
 else ifeq ($(TOOLCHAIN),gnu)
-	LDFLAGS += -lopenblas -lpthread
+	ifeq ($(OS),Windows_NT)
+		LDFLAGS += -lblas -llapack
+	else
+		LDFLAGS += -lopenblas -lpthread
+	endif
+else ifeq ($(TOOLCHAIN),sgi)
+	LDFLAGS = -L $(SUITESPARSE_LIB) -L $(SUPERLU_LIB) -lsuperlu -lumfpack -lamd -lcholmod \
+          -lcolamd -lcamd -lccolamd -lblas
 endif
 
 
 # source files and objects
-SRC = from_nrtype.f90 orbit_mod.f90 mesh_mod.f90 \
+SRC = from_nrtype.f90 spline5_RZ.f90 field_divB0.f90 bdivfree.f90 orbit_mod.f90 mesh_mod.f90 \
       sparse_mod.f90 magdif.f90 magdif_test.f90
 SRCS = $(patsubst %.f90, %.o, $(SRC))
 
@@ -69,4 +91,4 @@ umf4_f77zwrapper.o: $(SUITESPARSE_F90)/umf4_f77zwrapper.c
 	$(CC) $(CCFLAGS) -I $(SUITESPARSE_HDR) -DZLONG -o $@ $^
 
 clean:
-	rm -f *.o *.mod
+	rm -f *.o *.mod magdif_test magdif_test.exe
--- magdif.f90
+++ magdif.f90
@@ -1,28 +1,63 @@
 module magdif
+  use from_nrtype
+  use sparse_mod, only: remap_rc
   implicit none
   
   contains
   
-  subroutine assemble_system(n, a, b, c, q, d, du, alpha, Mq)
-    integer, intent(in) :: n                   ! number of system rows
-    complex(8), intent(in) :: a(:), b(:), c(:) ! system coefficients
-    complex(8), intent(in) :: q(:)             ! right-hand side without mass matrix applied
-    complex(8), intent(out) :: d(n)            ! diagonal of stiffness matrix
-    complex(8), intent(out) :: du(n-1)         ! superdiagonal of stiffness matrix
-    complex(8), intent(out) :: alpha           ! single entry in stiffness matrix to be periodic
-    complex(8), intent(out) :: Mq(:)           ! right-hand side with mass matrix applied
+  subroutine assemble_system(nrow, a, b, c, q, d, du, Mq)
+    integer, intent(in) :: nrow                ! number of system rows
+    complex(dp), intent(in) :: a(:), b(:), c(:) ! system coefficients
+    complex(dp), intent(in) :: q(:)             ! right-hand side without mass matrix applied
+    complex(dp), intent(out) :: d(nrow)         ! diagonal of stiffness matrix
+    complex(dp), intent(out) :: du(nrow)        ! superdiagonal of stiffness matrix + A(n,1)
+    complex(dp), intent(out) :: Mq(:)           ! right-hand side with mass matrix applied
 
     integer :: k
 
-    do k=1,n-1
+    do k=1,nrow-1
        d(k) = -a(k) + b(k)/2d0
        du(k) = a(k) + b(k)/2d0
        Mq(k) = (c(k+1)*q(k+1)+c(k)*q(k))/2d0
     end do
-    d(n) = -a(n) + b(n)/2d0
-    Mq(n) = (c(1)*q(1)+c(n)*q(n))/2d0
-    alpha = a(1) + b(1)/2d0
+    
+    d(nrow) = -a(nrow) + b(nrow)/2d0
+    du(nrow) = a(1) + b(1)/2d0
+    Mq(nrow) = (c(1)*q(1)+c(nrow)*q(nrow))/2d0
   end subroutine assemble_system
+
+  subroutine assemble_sparse(nrow, d, du, nz, irow, icol, aval)
+    integer, intent(in)  :: nrow                          ! number of system rows
+    complex(dp), intent(in)  :: d(nrow)                   ! diagnonal
+    complex(dp), intent(in)  :: du(nrow)                  ! upper diagonal
+    integer, intent(out) :: nz                            ! number of system rows
+    integer, intent(out) :: irow(2*nrow), icol(2*nrow)    ! matrix index representation
+    complex(dp), intent(out) :: aval(2*nrow)              ! matrix values
+    
+    integer :: k
+    
+    irow(1) = 1
+    icol(1) = 1
+    aval(1) = d(1)
+ 
+    irow(2) = nrow
+    icol(2) = 1
+    aval(2) = du(nrow)
+
+    do k = 2,nrow
+       ! off-diagonal 
+       irow(2*k-1) = k-1
+       icol(2*k-1) = k
+       aval(2*k-1) = du(k-1)
+
+       ! diagonal 
+       irow(2*k) = k
+       icol(2*k) = k
+       aval(2*k) = d(k)
+    end do
+
+    nz = 2*nrow
+  end subroutine assemble_sparse
 
   subroutine solve_full(n, d, du, alpha, Mq)
     integer, intent(in) :: n                   ! number of system rows
@@ -51,3 +86,4 @@ module magdif
     ! TODO: implement
   end subroutine solve_sparse
 end module magdif
+
