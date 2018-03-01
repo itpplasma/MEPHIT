795eadc7d2e950fc25223a0d0bd080be92869028
Christopher Albert <albert@tugraz.at>
2018-03-01 21:29:16 +0100

    gitignore and mesh preparation

--- PRELOAD/prepare_mesh.sh
+++ PRELOAD/prepare_mesh.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 gfortran -o tri_mesh.x SRC/tri_mesh.f90
-make -f Axis.mk && ./axis.x && ./tri_mesh.x && ./readcarre_m.x && ./writeout.x
+make -f Axis.mk
+make -f RC.mk
+make -f Writeout.mk
+./axis.x && ./tri_mesh.x && ./readcarre_m.x && ./writeout.x
 python extmesh.py inputformaxwell
 
