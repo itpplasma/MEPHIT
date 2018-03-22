5c398db1137083ec7e8d97ad7be20d889f3d39b0
Christopher Albert <albert@tugraz.at>
2018-03-22 12:28:52 +0100

    coding standard + doxygen

--- MHD/SRC/magdif.f90
+++ MHD/SRC/magdif.f90
@@ -1,33 +1,37 @@
 module magdif
   use from_nrtype, only: dp                                    ! PRELOAD/SRC/from_nrtype.f90
-  use mesh_mod, only: npoint, ntri, mesh_point, mesh_element,& ! PRELOAD/SRC/mesh_mod.f90 
+  use mesh_mod, only: npoint, ntri, mesh_point, mesh_element,& ! PRELOAD/SRC/mesh_mod.f90
        mesh_element_rmp, bphicovar
-  
+
   implicit none
 
   integer log_level
-  character (len=1024) :: point_file, tri_file  ! points.dat and triangles.dat from PRELOAD
+  character(len=1024) :: point_file, tri_file  ! points.dat and triangles.dat from PRELOAD
   logical :: log_err, log_warn, log_info, log_debug
 
   namelist / settings / log_level, point_file, tri_file
-  
-  integer, parameter :: log = 6                 ! log to stdout, TODO: make this configurable 
+
+  integer, parameter :: logfile = 6             ! log to stdout, TODO: make this configurable
 
   complex(dp), allocatable :: presn(:)          ! pressure perturbation p_n in each mesh point
   complex(dp), allocatable :: curr(:,:)         ! edge currents R j_n \cdot n weighted by R
   complex(dp), allocatable :: Bnflux(:,:)       ! edge fluxes R B_n \cdot n weighted by R
-  complex(dp), allocatable :: Bnphi(:)          ! physical toroidal component of Bn   
-  
+  complex(dp), allocatable :: Bnphi(:)          ! physical toroidal component of Bn
+
 contains
-  
+
   subroutine magdif_init
-    call read_config
+    call read_config('magdif.in')
     call read_mesh
   end subroutine magdif_init
-  
-  subroutine read_config
-    open(1, file='magdif.in')
-    read(1, nml=settings)
+
+  !> Read configuration file for magdif
+  !! @param config_filename file name of config file
+  subroutine read_config(config_filename)
+    character(len=*) :: config_filename
+
+    open(1, file = config_filename)
+    read(1, nml = settings)
     close(1)
 
     log_err = .false.
@@ -39,19 +43,19 @@ contains
     if (log_level>2) log_info = .true.
     if (log_level>3) log_debug = .true.
   end subroutine read_config
-  
+
   subroutine read_mesh
-    open(1, file=point_file, form='unformatted')
+    open(1, file = point_file, form = 'unformatted')
     read(1) npoint
-    if (log_info) write(log,*) 'npoint = ', npoint
+    if (log_info) write(logfile,*) 'npoint = ', npoint
     allocate(mesh_point(npoint))
     allocate(presn(npoint))
     read(1) mesh_point
     close(1)
 
-    open(1, file=tri_file, form='unformatted')
+    open(1, file = tri_file, form = 'unformatted')
     read(1) ntri
-    if (log_info) write(log,*) 'ntri   = ', ntri
+    if (log_info) write(logfile,*) 'ntri   = ', ntri
     allocate(mesh_element(ntri))
     allocate(mesh_element_rmp(ntri))
     allocate(Bnflux(ntri,3))
@@ -61,5 +65,5 @@ contains
     read(1) bphicovar
     close(1)
   end subroutine read_mesh
-  
+
 end module magdif
