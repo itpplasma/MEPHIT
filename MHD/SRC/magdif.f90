08b547e14550eeb9e36f8f6b518c3a206c718855
Patrick Lainer <patrick.lainer@student.tugraz.at>
2018-04-16 15:39:30 +0200

    file names read in via command line; literal values as named constants; matrix assembly corrected and simplified; comments redone; code beautifying

--- MHD/SRC/magdif.f90
+++ MHD/SRC/magdif.f90
@@ -1,6 +1,6 @@
 module magdif
-  use from_nrtype, only: dp                                    ! PRELOAD/SRC/from_nrtype.f90
-  use mesh_mod, only: npoint, ntri, mesh_point, mesh_element,& ! PRELOAD/SRC/mesh_mod.f90
+  use from_nrtype, only: dp                                     ! PRELOAD/SRC/from_nrtype.f90
+  use mesh_mod, only: npoint, ntri, mesh_point, mesh_element, & ! PRELOAD/SRC/mesh_mod.f90
        mesh_element_rmp, bphicovar, knot
   use for_macrostep, only : t_min, d_min
   use sparse_mod, only: remap_rc, sparse_solve, sparse_matmul
@@ -8,15 +8,19 @@ module magdif
   implicit none
 
   integer log_level
-  character(len=1024) :: point_file, tri_file       ! points.dat and triangles.dat from PRELOAD
   logical :: log_err, log_warn, log_info, log_debug ! specify log levels
+
+  character(len=1024) :: point_file, tri_file, Bnflux_file, hpsi_file ! data files from PRELOAD
+  character(len=1024) :: config_file ! config file for namelist
+  character(len=1024) :: presn_file ! output files
 
   integer  :: n               ! harmonic index of perturbation
   integer  :: nkpol           ! number of knots per poloidal loop
   integer  :: nflux           ! number of flux surfaces
-  real(dp) :: ti0, di0        ! axis value and slope of temperature T = ti0 + di0*(psi-psi_ax)
-  
-  namelist / settings / log_level, point_file, tri_file, n, nkpol, nflux, ti0, di0
+  real(dp) :: ti0, di0        ! interpolation steps for temperature and density
+
+  namelist / settings / log_level, point_file, tri_file, Bnflux_file, hpsi_file, presn_file, &
+       n, nkpol, nflux, ti0, di0
 
   integer, parameter :: logfile = 6             ! log to stdout, TODO: make this configurable
 
@@ -28,18 +32,21 @@ module magdif
   complex(dp), allocatable :: Bnphi(:)          ! physical toroidal component of Bn
 
   real(dp) :: psimin, psimax, dens, temp
+  real(dp), parameter :: R0 = 172.74467899999999d0 ! distance of magnetic axis from center
+  real(dp), parameter :: ideal_gas_factor = 1.6021766208d-12  ! unit conversion factor in ideal gas law (p = N/V * k_B*T) with p in dyn cm^-1, N/V in cm^-3 and k_B*T in eV
+  complex(dp), parameter :: imun = (0.0_dp, 1.0_dp) ! imaginary unit in double precision
 
 contains
 
   !> Initialize magdif module
   subroutine magdif_init
-    call read_config('magdif.in')
+    call read_config(config_file)
     call read_mesh
     call read_bnflux
     call read_hpsi ! TODO: get rid of this due to redundancy with bnflux
-    call init_flux_variables    
+    call init_flux_variables
     !call init_safety_factor ! TODO
-    if (log_info) write(logfile,*) 'magdif initialized'
+    if (log_info) write(logfile, *) 'magdif initialized'
   end subroutine magdif_init
 
   !> Final cleanup of magdif module
@@ -53,13 +60,13 @@ contains
     deallocate(mesh_point)
     deallocate(mesh_element)
     deallocate(mesh_element_rmp)
-    if (log_info) write(logfile,*) 'magdif cleanup finished'
+    if (log_info) write(logfile, *) 'magdif cleanup finished'
   end subroutine magdif_cleanup
 
   !> Read configuration file for magdif
   !! @param config_filename file name of config file
   subroutine read_config(config_filename)
-    character(len=*) :: config_filename
+    character(len = *) :: config_filename
 
     open(1, file = config_filename)
     read(1, nml = settings)
@@ -69,17 +76,17 @@ contains
     log_warn = .false.
     log_info = .false.
     log_debug = .false.
-    if (log_level>0) log_err = .true.
-    if (log_level>1) log_warn = .true.
-    if (log_level>2) log_info = .true.
-    if (log_level>3) log_debug = .true.
+    if (log_level > 0) log_err = .true.
+    if (log_level > 1) log_warn = .true.
+    if (log_level > 2) log_info = .true.
+    if (log_level > 3) log_debug = .true.
   end subroutine read_config
 
   !> Read mesh points and triangles
   subroutine read_mesh
     open(1, file = point_file, form = 'unformatted')
     read(1) npoint
-    if (log_info) write(logfile,*) 'npoint = ', npoint
+    if (log_info) write(logfile, *) 'npoint = ', npoint
     allocate(mesh_point(npoint))
     allocate(presn(npoint))
     read(1) mesh_point
@@ -87,17 +94,17 @@ contains
 
     open(1, file = tri_file, form = 'unformatted')
     read(1) ntri
-    if (log_info) write(logfile,*) 'ntri   = ', ntri
+    if (log_info) write(logfile, *) 'ntri   = ', ntri
     allocate(mesh_element(ntri))
     read(1) mesh_element
     read(1) bphicovar
     close(1)
 
     allocate(mesh_element_rmp(ntri))
-    allocate(Bnflux(ntri,3))
+    allocate(Bnflux(ntri, 3))
     allocate(Bnphi(ntri))
-    allocate(currn(ntri,3))
-    
+    allocate(currn(ntri, 3))
+
     Bnflux = 0d0
     Bnphi = 0d0
     currn = 0d0
@@ -107,18 +114,19 @@ contains
   subroutine read_bnflux
     integer :: k
     real(dp) :: dummy8(8)
-    
-    open(1, file = 'VACFIELD/Bn_flux.dat')
-    do k = 1, ntri
-       read(1,*) dummy8
-       Bnflux(k,1) = dummy8(1) + (0d0,1d0)*dummy8(2)
-       Bnflux(k,2) = dummy8(3) + (0d0,1d0)*dummy8(4)
-       Bnflux(k,3) = dummy8(5) + (0d0,1d0)*dummy8(6)
-       Bnphi(k) = dummy8(7) + (0d0,1d0)*dummy8(8)
 
-       if(abs((sum(Bnflux(k,:))+&
-            (0d0,1d0)*n*Bnphi(k)*mesh_element(k)%det_3/2d0)/sum(Bnflux(k,:))) > 1d-10) then
-          stop 'Bnvac not divergence free'
+    open(1, file = Bnflux_file)
+    do k = 1, ntri
+       read(1, *) dummy8
+       Bnflux(k,1) = cmplx(dummy8(1), dummy8(2), dp)
+       Bnflux(k,2) = cmplx(dummy8(3), dummy8(4), dp)
+       Bnflux(k,3) = cmplx(dummy8(5), dummy8(6), dp)
+       Bnphi(k) = cmplx(dummy8(7), dummy8(8), dp)
+
+       if (abs((sum(Bnflux(k,:)) + imun * n * Bnphi(k) * mesh_element(k)%det_3 * 0.5d0) &
+            / sum(Bnflux(k,:))) > 1d-10) then
+          if (log_err) write(logfile, *) 'vacuum B_n not divergence free'
+          stop 'vacuum B_n not divergence free'
        end if
     end do
     close(1)
@@ -128,15 +136,19 @@ contains
   subroutine read_hpsi
     integer :: k
     real(dp) :: dummy_re, dummy_im
-    
-    open(1, file='VACFIELD/hpsi_vac.dat')
-    do k=1, ntri
-       read (1,*) dummy_re, dummy_im
-       mesh_element_rmp(k)%bnorm_vac=cmplx(dummy_re,dummy_im,dp)
+
+    open(1, file = hpsi_file)
+    do k = 1, ntri
+       read (1, *) dummy_re, dummy_im
+       !TODO: enable next line
+       !mesh_element_rmp(k)%bnorm_vac = cmplx(dummy_re, dummy_im, dp)
        !mesh_element_rmp(k)%bnorm_vac = 1d0 ! check with constant source
-       !mesh_element_rmp(k)%bnorm_vac = 3.d0*172.74467899999999*abs(bphicovar) &
-       !     /sum(mesh_point(mesh_element(k)%i_knot(:))%rcoord**2 &
-       !     *mesh_point(mesh_element(k)%i_knot(:))%b_mod) ! check 2
+       !
+       !Test case: completely non-resonant perturbation
+       !TODO: disable this line
+       mesh_element_rmp(k)%bnorm_vac = 3.d0 * R0 * abs(bphicovar) &
+            / sum(mesh_point(mesh_element(k)%i_knot(:))%rcoord ** 2 &
+            * mesh_point(mesh_element(k)%i_knot(:))%b_mod)
     end do
     close(1)
   end subroutine read_hpsi
@@ -144,7 +156,7 @@ contains
   !>Initialize poloidal psi and unperturbed pressure p0
   subroutine init_flux_variables
     integer :: k
-    
+
     psimin = minval(mesh_point%psi_pol)
     psimax = maxval(mesh_point%psi_pol)
 
@@ -153,39 +165,28 @@ contains
     allocate(pres0(nflux+1))
     allocate(psi(nflux+1))
 
-    do k = 0, nflux
-       if(k==0) then
+    do k = 0, nflux  ! magnetic axis at k == 0 is not counted as flux surface
+       if (k == 0) then
           psi(1) = mesh_point(1)%psi_pol
        else
           psi(k+1) = mesh_point(1 + (k-1) * nkpol + 1)%psi_pol
        endif
-       dens = (psi(k+1) - psimin) / psimax * di0 + d_min !dens
-       temp = (psi(k+1) - psimin) / psimax * ti0 + t_min !temp
-       pres0(k+1) = dens * temp * 1.602176d-12
-    end do 
+       dens = (psi(k+1) - psimin) / psimax * di0 + d_min
+       temp = (psi(k+1) - psimin) / psimax * ti0 + t_min
+       pres0(k+1) = dens * temp * ideal_gas_factor
+    end do
   end subroutine init_flux_variables
 
   !>TODO
-  subroutine assemble_system(nrow, a, b, c, q, d, du, Mq)
+  subroutine assemble_system_first_order(nrow, a, b, d, du)
     integer, intent(in) :: nrow                 ! number of system rows
-    complex(dp), intent(in) :: a(:), b(:), c(:) ! system coefficients
-    complex(dp), intent(in) :: q(:)             ! right-hand side without mass matrix applied
+    complex(dp), intent(in), dimension(nrow) :: a, b ! system coefficients
     complex(dp), intent(out) :: d(nrow)         ! diagonal of stiffness matrix
     complex(dp), intent(out) :: du(nrow)        ! superdiagonal of stiffness matrix + A(n,1)
-    complex(dp), intent(out) :: Mq(:)           ! right-hand side with mass matrix applied
 
-    integer :: k
-
-    do k=1,nrow-1
-       d(k) = -a(k) + b(k)/2d0
-       du(k) = a(k) + b(k)/2d0
-       Mq(k) = (c(k+1)*q(k+1)+c(k)*q(k))/2d0
-    end do
-    
-    d(nrow) = -a(nrow) + b(nrow)/2d0
-    du(nrow) = a(1) + b(1)/2d0
-    Mq(nrow) = (c(1)*q(1)+c(nrow)*q(nrow))/2d0
-  end subroutine assemble_system
+    d = -a + b * 0.5d0
+    du = a + b * 0.5d0
+  end subroutine assemble_system_first_order
 
   !>TODO
   subroutine assemble_sparse(nrow, d, du, nz, irow, icol, aval)
@@ -195,24 +196,24 @@ contains
     integer, intent(out) :: nz                            ! number of system rows
     integer, intent(out) :: irow(2*nrow), icol(2*nrow)    ! matrix index representation
     complex(dp), intent(out) :: aval(2*nrow)              ! matrix values
-    
+
     integer :: k
-    
+
     irow(1) = 1
     icol(1) = 1
     aval(1) = d(1)
- 
+
     irow(2) = nrow
     icol(2) = 1
     aval(2) = du(nrow)
 
     do k = 2,nrow
-       ! off-diagonal 
+       ! off-diagonal
        irow(2*k-1) = k-1
        icol(2*k-1) = k
        aval(2*k-1) = du(k-1)
 
-       ! diagonal 
+       ! diagonal
        irow(2*k) = k
        icol(2*k) = k
        aval(2*k) = d(k)
@@ -220,7 +221,7 @@ contains
 
     nz = 2*nrow
   end subroutine assemble_sparse
-  
+
   !>TODO
   subroutine common_triangles(knot1, knot2, common_tri)
     type(knot), intent(in) :: knot1, knot2
@@ -228,21 +229,21 @@ contains
 
     integer :: k, l, kcom
     kcom = 0
-    do k=1,knot1%n_owners
-       do l=1,knot2%n_owners
-          if (knot1%i_owner_tri(k)==knot2%i_owner_tri(l)) then
+    do k = 1, knot1%n_owners
+       do l = 1, knot2%n_owners
+          if (knot1%i_owner_tri(k) == knot2%i_owner_tri(l)) then
              kcom = kcom+1
-             if (kcom>2) stop "Error: more than two common triangles for knots"
+             if (kcom > 2) stop "Error: more than two common triangles for knots"
              common_tri(kcom) = knot1%i_owner_tri(k)
           end if
        end do
     end do
   end subroutine common_triangles
-  
+
   !>Compute pressure perturbation. TODO: cleanup and documentation
   subroutine compute_presn
-    real(dp) :: r,p,z,Br,Bp,Bz,dBrdR,dBrdp,dBrdZ         &
-         ,dBpdR,dBpdp,dBpdZ,dBzdR,dBzdp,dBzdZ
+    real(dp) :: r, p, z, Br, Bp, Bz, dBrdR, dBrdp, dBrdZ, &
+         dBpdR, dBpdp, dBpdZ, dBzdR, dBzdp, dBzdZ
     real(dp) :: rold, zold, Brold, Bpold, Bzold ! previous values in loop
     complex(dp), dimension(nkpol) :: qn, a, b, c, d, du, x, Mq
     integer :: kp, kl
@@ -254,11 +255,11 @@ contains
     integer :: common_tri(2)
 
     r = 0d0; p = 0d0; z = 0d0
-    
-    call field(r,p,z,Br,Bp,Bz,dBrdR,dBrdp,dBrdZ          &
-         ,dBpdR,dBpdp,dBpdZ,dBzdR,dBzdp,dBzdZ)
 
-    open(1,file='test_pressure_x.out', recl=1024)
+    call field(r, p, z, Br, Bp, Bz, dBrdR, dBrdp, dBrdZ, &
+         dBpdR, dBpdp, dBpdZ, dBzdR, dBzdp, dBzdZ)
+
+    open(1, file = presn_file, recl = 1024)
     do kl = 1, nflux ! loop through flux surfaces
       do kp = 1, nkpol ! loop through poloidal points along ring
         rold = r; zold = z
@@ -268,31 +269,31 @@ contains
         curknot = mesh_point(1 + (kl-1)*nkpol + kp)
         r = curknot%rcoord; p = 0d0; z = curknot%zcoord
 
-        call field(r,p,z,Br,Bp,Bz,dBrdR,dBrdp,dBrdZ          &
-             ,dBpdR,dBpdp,dBpdZ,dBzdR,dBzdp,dBzdZ)
+        call field(r, p, z, Br, Bp, Bz, dBrdR, dBrdp, dBrdZ, &
+             dBpdR, dBpdp, dBpdZ, dBzdR, dBzdp, dBzdZ)
 
         psi_loc = curknot%psi_pol
-        dens_prof=(psi_loc - psimin)/psimax*di0 + d_min
-        ddens_dpsi=1.d0/psimax*di0
-        temp_prof=(psi_loc - psimin)/psimax*ti0 + t_min
-        dtemp_dpsi=1.d0/psimax*ti0
+        dens_prof = (psi_loc - psimin) / psimax * di0 + d_min
+        ddens_dpsi = di0 / psimax
+        temp_prof = (psi_loc - psimin) / psimax * ti0 + t_min
+        dtemp_dpsi = ti0 / psimax
 
-        c(kp) = -(ddens_dpsi*temp_prof + dens_prof*dtemp_dpsi)*1.602176d-12 ! -dp0/dpsi
+        c(kp) = -ideal_gas_factor * (ddens_dpsi*temp_prof + dens_prof*dtemp_dpsi) ! -dp0/dpsi
 
-        if(kp==1) cycle ! first point
+        if (kp == 1) cycle ! first point
 
         call common_triangles(oldknot, curknot, common_tri)
 
-        call field(r,p,z,Br,Bp,Bz,dBrdR,dBrdp,dBrdZ          &
-             ,dBpdR,dBpdp,dBpdZ,dBzdR,dBzdp,dBzdZ)
-        
-        x(kp-1) = c(kp-1)*(oldknot%b_mod+curknot%b_mod)/2d0* &
-             sum(mesh_element_rmp(common_tri(:))%bnorm_vac)/2d0
+        call field(r, p, z, Br, Bp, Bz, dBrdR, dBrdp, dBrdZ, &
+             dBpdR, dBpdp, dBpdZ, dBzdR, dBzdp, dBzdZ)
 
-        a(kp-1)=((Br+Brold)/2d0*(r-rold) &
-             +(Bz+Bzold)/2d0*(z-zold))/((r-rold)**2+(z-zold)**2)
+        x(kp-1) = c(kp-1) * (oldknot%b_mod + curknot%b_mod) * 0.5d0 * &
+             sum(mesh_element_rmp(common_tri(:))%bnorm_vac) * 0.5d0
 
-        b(kp-1)=(0d0,1d0)*n*(Bp/r+Bpold/rold)/2d0
+        a(kp-1) = ((Br + Brold) * 0.5d0 * (r-rold) + &
+             (Bz + Bzold) * 0.5d0 * (z - zold)) / ((r - rold) ** 2 + (z - zold) ** 2)
+
+        b(kp-1) = imun * n * (Bp / r + Bpold / rold) * 0.5d0
       end do ! kp
 
        ! once more between last and first point
@@ -305,36 +306,36 @@ contains
 
        r = curknot%rcoord; p = 0d0; z = curknot%zcoord
 
-       call field(r,p,z,Br,Bp,Bz,dBrdR,dBrdp,dBrdZ          &
-            ,dBpdR,dBpdp,dBpdZ,dBzdR,dBzdp,dBzdZ)
+       call field(r, p, z, Br, Bp, Bz, dBrdR, dBrdp, dBrdZ, &
+            dBpdR, dBpdp, dBpdZ, dBzdR, dBzdp, dBzdZ)
 
        call common_triangles(oldknot, curknot, common_tri)
-       x(nkpol) = c(nkpol)*(oldknot%b_mod+curknot%b_mod)/2d0* &
-            sum(mesh_element_rmp(common_tri(:))%bnorm_vac)/2d0
+       x(nkpol) = c(nkpol) * (oldknot%b_mod + curknot%b_mod) * 0.5d0 * &
+            sum(mesh_element_rmp(common_tri(:))%bnorm_vac) * 0.5d0
 
-       a(nkpol)=((Br+Brold)/2d0*(r-rold) &
-            +(Bz+Bzold)/2d0*(z-zold))/((r-rold)**2+(z-zold)**2)
-       b(nkpol)=(0d0,1d0)*n*(Bp/r+Bpold/rold)/2d0
+       a(nkpol) = ((Br + Brold) * 0.5d0 * (r - rold) + &
+            (Bz + Bzold) * 0.5d0 * (z - zold)) / ((r - rold) ** 2 + (z - zold) ** 2)
+       b(nkpol) = imun * n * (Bp / r + Bpold / rold) * 0.5d0
 
        ! solve linear system
-       call assemble_system(nkpol, a, b, c, qn, d, du, Mq)
+       call assemble_system_first_order(nkpol, a, b, d, du)
        call assemble_sparse(nkpol, d, du, nz, irow, icol, aval)
-       call sparse_solve(nkpol,nkpol,nz,irow,icol,aval,x)
+       call sparse_solve(nkpol, nkpol, nz, irow, icol, aval, x)
 
-       if(kl==1) then ! first point on axis before actual output
-       write(1,*) mesh_point(1)%psi_pol, dens_prof, temp_prof,&
-            temp_prof*dens_prof*1.602176d-12, real(a(nkpol)), Bp,&
-            real(sum(x)/size(x)), aimag(sum(x)/size(x))!, 0d0, 0d0
+       if (kl == 1) then ! first point on axis before actual output
+       write(1, *) mesh_point(1)%psi_pol, dens_prof, temp_prof, &
+            temp_prof * dens_prof * ideal_gas_factor, real(a(nkpol)), Bp, &
+            real(sum(x) / size(x)), aimag(sum(x) / size(x)) !, 0d0, 0d0
        end if
-       do kp=1,nkpol
-          presn((kl-1)*nkpol+1 + kp) = x(kp)
-          write(1,*) mesh_point((kl-1)*nkpol+1 + kp)%psi_pol, dens_prof,&
-            temp_prof,temp_prof*dens_prof*1.602176d-12, real(a(nkpol)),&
-            Bp, real(x(kp)), aimag(x(kp))!, real(a(kp)), aimag(a(kp))
+       do kp = 1, nkpol
+          presn((kl - 1) * nkpol + 1 + kp) = x(kp)
+          write(1, *) mesh_point((kl - 1) * nkpol + 1 + kp)%psi_pol, dens_prof, &
+            temp_prof, temp_prof * dens_prof * ideal_gas_factor, real(a(nkpol)), &
+            Bp, real(x(kp)), aimag(x(kp)) !, real(a(kp)), aimag(a(kp))
        end do
     end do ! kl
-    do kp=1,(npoint - nkpol*nflux - 1) ! write zeroes in remaining points until end
-       write(1,*) 0d0, 0d0, 0d0, 0d0, 0d0, 0d0, 0d0, 0d0
+    do kp = 1, (npoint - nkpol * nflux - 1) ! write zeroes in remaining points until end
+       write(1, *) 0d0, 0d0, 0d0, 0d0, 0d0, 0d0, 0d0, 0d0
     end do
     close(1)
   end subroutine compute_presn
