f0530ec4f03e80142b3ee44dc7280a7384856615
Christopher Albert <albert@alumni.tugraz.at>
2018-06-12 15:42:27 +0200

    killing some warnings

--- MHD/SCRIPTS/simple_fdm.py
+++ MHD/SCRIPTS/simple_fdm.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 ns = 10
 ds = 2*np.pi/ns
 
-s = np.linspace(0,2*np.pi-ds,ns)
+s = np.linspace(0, 2*np.pi-ds, ns)
 n = 1
 m = 1
 
@@ -16,20 +16,20 @@ pn = pmn*np.exp(1.0j*m*s)
 dsinv = 1.0/ds
 diag1 = np.ones(ns)*(0.5j*n-dsinv)
 diag2 = np.ones(ns-1)*(0.5j*n+dsinv)
-A = np.diag(diag1) + np.diag(diag2,1)
-A[-1,0] = (0.5j*n+dsinv)
+A = np.diag(diag1) + np.diag(diag2, 1)
+A[-1, 0] = (0.5j*n+dsinv)
 
-qnnum = 0.5*(np.roll(qn,-1)+qn)
-pnnum = np.linalg.solve(A,qnnum)
+qnnum = 0.5*(np.roll(qn, -1)+qn)
+pnnum = np.linalg.solve(A, qnnum)
 
 plt.figure()
 plt.plot(pn)
 plt.plot(qn)
 plt.plot(pnnum, 'x')
 
-x = np.loadtxt('test_fdm_simple_x.out')
-plt.plot(x[:,0], 'o')
-plt.show()
+# x = np.loadtxt('test_fdm_simple_x.out')
+# plt.plot(x[:,0], 'o')
+# rtri = np.loadtxt('test_triangle_strip.out')
+# plt.plot(rtri[:,0],rtri[:,1],'x')
 
-rtri = np.loadtxt('test_triangle_strip.out')
-plt.plot(rtri[:,0],rtri[:,1],'x')
+plt.show()
