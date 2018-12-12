f4933faa8c4ca358c2bcb0f21f23419818b1c084
Christopher Albert <albert@alumni.tugraz.at>
2018-12-12 11:26:22 +0100

    working python version of maxwell, output still needed

--- FEM/maxwell.py
+++ FEM/maxwell.py
@@ -11,9 +11,25 @@ import matplotlib.pyplot as plt
 import codecs
 from io import StringIO
 import dolfin as df
-from dolfin import grad, div, curl, inner, dx
+from dolfin import derivative, div, curl, inner, dx
 
 df.parameters["reorder_dofs_serial"] = False
+
+doplot = True
+
+c = 29979245800.0
+
+n = 2.0
+
+#infile = sys.argv[1]
+#infile = '../PRELOAD/inputformaxwell.msh'
+infile = '../PRELOAD/inputformaxwell_ext.msh'
+currnfile = '../MHD/currn.dat'
+plotcurrnfile = '../MHD/plot_currn.dat'
+#currnfile = '../MHD/Bn_nonres.dat'
+#plotcurrnfile = '../MHD/plot_Bn_nonres.dat'
+
+#currnfile = '../PRELOAD/VACFIELD/Bn_flux.dat'
 
 # read msh file
 def readmsh(infile):
@@ -35,28 +51,10 @@ def readmsh(infile):
     elab = edge[:,2]
     edge = edge[:,0:2]
     
-    return [node, tri, edge, nlab, tlab, elab]
-
-def eps_permut(v):
-    if (v == [0,1,2] or v == [1,2,0] or v == [2,0,1]): return 1
-    if (v == [0,2,1] or v == [2,1,0] or v == [1,0,2]): return -1
-    return 0
-    
-
-#infile = sys.argv[1]
-#infile = '../PRELOAD/inputformaxwell.msh'
-infile = '../PRELOAD/inputformaxwell_ext.msh'
-currnfile = '../MHD/currn.dat'
-plotcurrnfile = '../MHD/plot_currn.dat'
+    return [node, tri, edge, nlab, tlab, elab]    
 
 [node, tri, edge, nlab, tlab, elab] = readmsh(infile)
 currn = np.loadtxt(currnfile)
-plotcurrn = np.loadtxt(plotcurrnfile)
-
-# plot mesh - without dolfin
-#plt.triplot(node[:, 0], node[:, 1], tri-1, linewidth=.1)
-#plt.axis('equal')
-#plt.show()
 
 mesh = df.Mesh()
 meshgen = df.MeshEditor()
@@ -69,14 +67,12 @@ for kt in range(len(tri)):
     meshgen.add_cell(kt, tri[kt,:]-1)
 meshgen.close()
 
-df.plot(mesh, linewidth=.1)
-#%%
-map2facing = [1,2,0] # index of edge facing vertex
-map2lex = np.argsort(tri,1) # map to lexical order in each triangle cell
+# Map for re-ordering of indexes and orientations
+map2facing = np.array([2,0,1]) # index of edge facing vertex
+map2lex = np.argsort(tri[:,map2facing],1) # map to lexical order in each triangle cell
 assert np.max(mesh.cells()[:]+1 - np.sort(tri)) == 0, "triangle sort order"
 
-#%%
-
+# Function spaces
 P0 = df.FunctionSpace(mesh, 'DG', 0)
 P1 = df.FunctionSpace(mesh, 'Lagrange', 1)
 Hdiv = df.FunctionSpace(mesh, 'RT', 1)
@@ -84,90 +80,142 @@ Hcurl = df.FunctionSpace(mesh, 'N1curl', 1)
 Hdivmap = Hdiv.dofmap()
 Hcurlmap = Hdiv.dofmap()
 
-# Test: plot p0 functions
-ftest = np.zeros(len(tri))
-ftest[0:len(plotcurrn)] = plotcurrn[:,0]
-
-#plot for comparison:
-#plt.figure()
-#plt.tripcolor(node[:, 0], node[:, 1], tri-1, ftest)
-
-plt.figure()
-f = df.Function(P0)
-f.vector()[:] = ftest
-df.plot(f)
-
-#%%
-
+# Read data
 cells = mesh.cells()
 nodes = mesh.coordinates()
+ndofs = len(Hdivmap.dofs())
 
-Jvec0 = np.zeros(len(Hdivmap.dofs()))
-J = df.Function(Hdiv)
-Jphi = df.Function(P0)
+Jvec = [np.zeros(ndofs), np.zeros(ndofs)]
+J = [df.Function(Hdiv), df.Function(Hdiv)]
+Jphi = [df.Function(P0), df.Function(P0)]
 
 for kcell in range(len(currn)):
     nodeind = cells[kcell]
     nodecoords = nodes[nodeind]
-    cellorient = np.sign(np.linalg.det(np.concatenate(([[1,1,1]],nodecoords.T))))
+    cellarea = .5*np.linalg.det(np.concatenate(([[1,1,1]],nodecoords.T)))
+    cellorient = np.sign(cellarea)
     inddiff = np.roll(nodeind,1)+(-1)*np.roll(nodeind,-1) # (-1) to convert uint to int
     orient = np.sign(inddiff)
     dofs = Hdivmap.cell_dofs(kcell) # DOFs in cell k
-    Jvec0[dofs] = cellorient*orient*currn[kcell,2*map2lex[kcell,map2facing]]
-    Jphi.vector()[kcell] = currn[kcell, 6]
+    for k in range(2): # real and imaginary part
+        Jvec[k][dofs] = cellorient*orient*currn[kcell,2*map2lex[kcell]+k]
+        Jphi[k].vector()[kcell] = currn[kcell, 6+k]/np.abs(cellarea)
+        
+    #print(np.sum(orient*Jvec[0][dofs])-n*Jphi[1].vector()[kcell]*cellarea)
+    #print(np.sum(orient*Jvec[1][dofs])+n*Jphi[0].vector()[kcell]*cellarea)
+        
+J[0].vector()[:] = Jvec[0]
+J[1].vector()[:] = Jvec[1]
     
-J.vector()[:] = Jvec0
-
-#%%
-
-plt.figure()
-df.plot(Jphi)
-
-plt.figure()
-df.plot(J)
-
-#%%
-
-# Ampere
+#%% Solve Ampere
 def boundary(_, on_boundary):
     return on_boundary
 
 u = df.TrialFunction(Hcurl)
 v = df.TestFunction(Hcurl)
 
+# Homogeneous Dirichlet boundary condition
 A_D = df.Constant((0, 0))
 bc = df.DirichletBC(Hcurl, A_D, boundary)
 
-#Tanalyt = df.Expression(('exp(-(pow(x[0]-150,2)+pow(x[1],2))/pow(100,2))'), degree=2)
-#T = df.interpolate(Tanalyt, P1)
-#J = curl(T)
+rweight = df.Expression('x[0]', degree=1)
 
-#plt.figure()
-#p = df.plot(T)
-#plt.colorbar(p)
+# variational bilinear form for 2D curl-curl harmonics (->stiffness matrix)
+a = (rweight*inner(curl(u), curl(v)) + n**2/rweight*inner(u,v)) * dx
 
-a = (inner(curl(u), curl(v)) + .1*inner(u,v)) * dx
-b = inner(J, v) * dx
+# linear form (->right-hand side vector)
+b = []
+b.append(4.0*np.pi/c*inner(J[0], v) * dx) # real part
+b.append(4.0*np.pi/c*inner(J[1], v) * dx) # imaginary part
 
-A = df.Function(Hcurl)
-df.solve(a == b, A, bc)
+A = [df.Function(Hcurl), df.Function(Hcurl)] # poloidal vector potential
+df.solve(a == b[0], A[0], bc) # real part
+df.solve(a == b[1], A[1], bc) # imaginary part
 
-plt.figure()
-p = df.plot(div(J))
-plt.colorbar(p)
+# TODO: conversion to B field and output of fluxes
 
-plt.figure()
-p = df.plot(A)
-plt.colorbar(p)
-
-plt.figure()
-p = df.plot(curl(A))
-plt.colorbar(p)
-
-plt.show()
-
-#solve Ampere([ax, ay], [wx, wy], solver = UMFPACK) = // defines the PDE
-#  int2d(Th)(x * (dx(wy) - dy(wx)) * (dx(ay) - dy(ax)))
-#  + int2d(Th)(n^2 / x * [wx, wy] '* [ax, ay])
-#  - int2d(Th)(4.0 * pi / c * [wx, wy] '* [Jr,Jz])
-#  + on(1, ax = 0, ay = 0);
+#%%   ------------------------ 
+###   Plotting and comparisons
+###   ------------------------
+if (doplot):
+    plotcurrn = np.loadtxt(plotcurrnfile)
+    
+    #%% plot mesh
+    plt.subplot(1,2,1)
+    plt.triplot(node[:, 0], node[:, 1], tri-1, linewidth=.1)
+    plt.title('mesh (triplot)')
+    plt.subplot(1,2,2)
+    df.plot(mesh, linewidth=.1)
+    plt.title('mesh (fenics)')
+    
+    #%%
+    plt.figure()
+    plt.subplot(1,2,1)
+    df.plot(J[0])
+    plt.title('Re J')
+    plt.subplot(1,2,2)
+    df.plot(J[1])
+    plt.title('Im J')
+    plt.tight_layout()
+    
+    #%%
+    plt.figure()
+    plt.subplot(1,2,1)
+    p = df.plot(div(J[0])-n*Jphi[1])
+    plt.colorbar(p)
+    plt.title('Re div J')
+    plt.subplot(1,2,2)
+    p = df.plot(div(J[1])+n*Jphi[0])
+    plt.colorbar(p)
+    plt.title('Im div J')
+    
+    
+    #%%
+    
+    Jphiplot = [np.zeros(len(tri)),np.zeros(len(tri))]
+    Jphiplot[0][0:len(plotcurrn)] = plotcurrn[:,6]
+    Jphiplot[1][0:len(plotcurrn)] = plotcurrn[:,7]
+    
+    plt.figure()
+    plt.tripcolor(node[:, 0], node[:, 1], tri-1, Jphiplot[0])
+    plt.colorbar()
+    plt.title('Re Jphi (tripcolor)')
+    
+    plt.figure()
+    plt.tripcolor(node[:, 0], node[:, 1], tri-1, Jphiplot[1])
+    plt.colorbar()
+    plt.title('Im Jphi (tripcolor)')
+    
+    plt.figure()
+    p=df.plot(Jphi[0])
+    plt.colorbar(p)
+    plt.title('Re Jphi')
+    plt.figure()
+    p=df.plot(Jphi[1])
+    plt.colorbar(p)
+    plt.title('Im Jphi')
+    
+    #%%
+    plt.figure()
+    plt.subplot(1,2,1)
+    p = df.plot(A[0])
+    plt.colorbar(p)
+    plt.title('Re A')
+    plt.subplot(1,2,2)
+    p = df.plot(A[1])
+    plt.colorbar(p)
+    plt.title('Im A')
+    plt.tight_layout()
+    
+    plt.figure()
+    plt.subplot(1,2,1)
+    p = df.plot(curl(A[0]))
+    plt.colorbar(p)
+    plt.title('Re Bphi')
+    plt.subplot(1,2,2)
+    p = df.plot(curl(A[1]))
+    plt.colorbar(p)
+    plt.title('Im Bphi')
+    plt.tight_layout()
+    
+    plt.show()
