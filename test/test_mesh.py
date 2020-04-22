fea3e7c7e16cc416a4315e49d62fe2b43e7a71ba
Christopher Albert <albert@alumni.tugraz.at>
2020-04-22 13:57:49 +0200

    Testing

--- test/test_mesh.py
+++ test/test_mesh.py
@@ -1,10 +1,19 @@
+import numpy as np
+import pytest
 from magdif_fffi import magdif_config, magdif
 
 # TODO: write tests
 # TODO: read config and initialize with fixtures
 
-def test_magdif_init():
+
+@pytest.fixture(scope='module', autouse=True)
+def setup():
     magdif_config.read_config()
-    print("read config")
+    print("Finished reading config")
     magdif.magdif_init()
-    assert True  # TODO: check something actually
+    print("Magnetic field initialized")
+
+def test_nan():
+    assert not np.isnan(magdif.b0flux).any()
+    assert not np.isnan(magdif.bnflux).any()
+    assert not np.isnan(magdif.bnphi).any()
