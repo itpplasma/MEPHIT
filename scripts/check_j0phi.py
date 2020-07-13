corrected signs, updated plots

scripts/check_j0phi.py
@@ -25,15 +25,15 @@ dyn_per_cm3_to_N_per_m3 = 10.0
 c_cgs = 2.9979246e+10
 statA_per_cm2_to_A_per_m2 = 1.0e+05 / c_cgs
 
-work_dir = '/home/patrick/git/NEO-EQ/run/geomint_30835'
+work_dir = '/home/patrick/git/NEO-EQ/run/ED6_30835_3200'
 data_gs = np.loadtxt(path.join(work_dir, 'j0_gs.dat'))
 data_amp = np.loadtxt(path.join(work_dir, 'j0_amp.dat'))
 data_prof = np.loadtxt(path.join(work_dir, 'cmp_prof.dat'))
 
 nflux = 91
 kt_max = np.empty((nflux + 1), dtype=int)
-kt_max[0] = 100
-kt_max[1:] = 200
+kt_max[0] = 200
+kt_max[1:] = 400
 kt_low = np.empty((nflux + 1), dtype=int)
 kt_low[0] = 0
 kt_low[1:] = np.cumsum(kt_max[:-1]) + kt_low[0]

scripts/check_q.py
@@ -21,20 +21,21 @@ canvas = (6.6, 3.6)
 res = 300
 thin = 0.5
 
-work_dir = '/home/patrick/git/NEO-EQ/run/geomint_TCFP'
+work_dir = '/home/patrick/git/NEO-EQ/run/ED6_30835_3200'
+
 data_step = np.loadtxt(path.join(work_dir, 'check_q_step.dat'))
 data_cont = np.loadtxt(path.join(work_dir, 'check_q_cont.dat'))
 
 plt.figure(figsize=canvas)
-for m in range(3, 12):
-    plt.axhline(m / 2000, lw=0.25 * thin, color='k')
 plt.plot(data_cont[:, 0], data_cont[:, 2], '-k', lw=thin, label='gfile')
 plt.plot(data_cont[:, 0], data_cont[:, 1], '--r', lw=thin, label='field line')
 plt.step(data_step[:, 0], data_step[:, 1], where='mid', lw=thin, label='triangle grid')
+plt.gca().yaxis.set_major_locator(matplotlib.ticker.MultipleLocator(0.5))
+plt.gca().grid(which='major', axis='y')
 # plt.xlim([55.6, 56.2])
 # plt.ylim([0.00448, 0.00452])
-plt.gca().legend(loc='lower right')
-plt.xlabel(r'$d$ / cm')
+plt.gca().legend()
+plt.xlabel(r'$r$ / cm')
 plt.ylabel(r'$q$')
 plt.title('Comparison of safety factor approximations')
 plt.savefig(path.join(work_dir, "check_q.png"), dpi=res)