b78cb8408d9b5195801ae8fe9e8a2e7135841fa4
Patrick Lainer <patrick.lainer@tugraz.at>
2024-03-28 16:42:35 +0100

    remove definitely unneeded files

--- scripts/old/exportfig.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-import os
-
-def exportfig(filename):
-    print('export: ' + filename)
-    plt.tight_layout()
-    plt.savefig(filename + '.pdf')
-    os.system('pdfcrop ' + filename + '.pdf ' + filename + '.pdf')
-    
--- scripts/old/maxwell_real.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-PATH=/temp/ert/local/bin:$PATH
-pushd ../FEM >/dev/null
-FreeFem++ maxwell_real.edp 1>/tmp/freefem.out 2>&1
-exitstat=$?
-popd >/dev/null
-exit $exitstat
