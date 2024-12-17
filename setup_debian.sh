7f6f05c28aa782f3d6802172c86535837454f929
Christopher Albert <albert@alumni.tugraz.at>
2024-12-17 18:05:18 +0000

    Add Python dependency

--- setup_debian.sh
+++ setup_debian.sh
@@ -2,5 +2,5 @@
 
 sudo apt-get update -y && sudo apt-get install -y -q --no-install-recommends \
     libopenblas-dev libsuitesparse-dev libmetis-dev libhdf5-dev \
-    libnetcdf-dev libnetcdff-dev libboost-dev \
-    libfftw3-dev openmpi-bin libopenmpi-dev
+    libnetcdf-dev libnetcdff-dev libboost-dev libfftw3-dev \
+    openmpi-bin libopenmpi-dev python3 python3-dev python3-pip python3-numpy
