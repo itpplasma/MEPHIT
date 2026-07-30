b93dfd9e0ff6902f1625bb034b5d831d0c8bd3db
Christopher Albert <albert@tugraz.at>
2026-07-30 18:13:55 +0200

    Replace system I/O dependencies with Fortio

--- setup/debian.sh
+++ setup/debian.sh
@@ -2,6 +2,6 @@
 
 sudo apt-get update -y && sudo apt-get install -y -q --no-install-recommends \
     cmake make ninja-build git gfortran gcc patch libgsl-dev \
-    libopenblas-dev libsuitesparse-dev libmetis-dev libhdf5-dev \
-    libnetcdf-dev libnetcdff-dev libboost-dev libfftw3-dev \
+    libopenblas-dev libsuitesparse-dev libmetis-dev zlib1g-dev \
+    libboost-dev libfftw3-dev \
     openmpi-bin libopenmpi-dev python3 python3-dev python3-pip python3-numpy
--- setup/mpcdf_viper.sh
+++ setup/mpcdf_viper.sh
@@ -5,8 +5,6 @@ module load ninja
 module load gcc
 module load openmpi
 module load mkl
-module load hdf5-serial
-module load netcdf-serial
 module load fftw-serial
 module load boost
 module load gsl
